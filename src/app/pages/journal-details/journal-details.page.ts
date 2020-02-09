c205728243211612b75227a9c287812a814f6833
Nicole Marty <nmarty310@gmail.com>
2020-02-09 13:58:12 -0600

    using Angular Router and ActivatedRoute and snapshot of current route to get id from params to see what should load and when

--- src/app/pages/journal-details/journal-details.page.ts
+++ src/app/pages/journal-details/journal-details.page.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Journal, JournalService } from './../../services/journal.service'
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { NavController, LoadingController } from '@ionic/angular'
 
 @Component({
   selector: 'app-journal-details',
   templateUrl: './journal-details.page.html',
-  styleUrls: ['./journal-details.page.scss'],
+  styleUrls: ['./journal-details.page.scss']
 })
 export class JournalDetailsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  journal: Journal = {
+    title: 'Title your post',
+    createdAt: new Date().getTime(),
+    content: 'Tell us about it'
   }
 
+  journalId = null
+
+  constructor (
+    private route: ActivatedRoute,
+    private nav: NavController,
+    private journalService: JournalService,
+    private loadingController: LoadingController
+  ) {}
+
+  ngOnInit () {
+    this.journalId = this.route.snapshot.params['id']
+    if (this.journalId) {
+      this.loadJournal()
+    }
+  }
+
+  async loadJournal () {
+    const loading = await this.loadingController.create({
+      message: 'Loading Journal..'
+    })
+    await loading.present()
+
+    this.journalService.getJournal(this.journalId).subscribe(res => {
+      loading.dismiss()
+      this.journal = res
+    })
+  }
+
+  async saveJournal () {
+    const loading = await this.loadingController.create({
+      message: 'Saving Journal..'
+    })
+    await loading.present()
+
+    if (this.journalId) {
+      this.journalService
+        .updateJournal(this.journal, this.journalId)
+        .then(() => {
+          loading.dismiss()
+          this.nav.back('home')
+        })
+    } else {
+      this.journalService.addJournal(this.journal).then(() => {
+        loading.dismiss()
+        this.nav.back('home')
+      })
+    }
+  }
 }
