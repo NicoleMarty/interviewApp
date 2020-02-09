48e09a903a439ca2c199ffcfc4fb0ccfefbb920a
Nicole Marty <nmarty310@gmail.com>
2020-02-09 13:26:33 -0600

    adding data to firebase from the journalService

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Journal, JournalService } from '../services/journal.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  journals: Journal[]
 
-  constructor() {}
+  constructor (private journalService: JournalService) {}
 
+  ngOnInit () {
+    this.journalService.getJournals().subscribe(res => {
+      this.journals = res
+    })
+  }
+
+  remove (item) {
+    this.journalService.removeJournal(item.id)
+  }
 }
