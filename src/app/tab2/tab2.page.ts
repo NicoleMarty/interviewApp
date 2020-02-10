148ef9a1f8391015e0d17820cbc49ea22e763b5a
Nicole Marty <nmarty310@gmail.com>
2020-02-10 11:11:34 -0600

    copied photo files from earlier tutorial to tab in project to use camera and store photos. can't actually take a photo yet because the camera isn't enabled, but saves and deletes photos'

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ActionSheetController } from '@ionic/angular'
+import { PhotoService } from '../services/photo.service'
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  constructor (
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) {}
 
-  constructor() {}
+  ngOnInit () {
+    this.photoService.loadSaved()
+  }
 
+  public async showActionSheet (photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.photoService.deletePicture(photo, position)
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            // Nothing to do, action sheet is automatically closed
+          }
+        }
+      ]
+    })
+    await actionSheet.present()
+  }
 }
