a08be808765dd79078245a11c4f759fc068c208c
Nicole Marty <nmarty310@gmail.com>
2020-02-09 19:29:43 -0600

    updating prod environment and required firebase files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDv2bR0nZWeB7DlraPz6IqAih0zwVDL4Mw',
+    authDomain: 'interviewapp-5b2db.firebaseapp.com',
+    databaseURL: 'https://interviewapp-5b2db.firebaseio.com',
+    projectId: 'interviewapp-5b2db',
+    storageBucket: 'interviewapp-5b2db.appspot.com',
+    messagingSenderId: '416875217824'
+  }
+}
