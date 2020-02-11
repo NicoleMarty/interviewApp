82578617931286db8c5e4f1478517995b4f74de6
Nicole Marty <nmarty310@gmail.com>
2020-02-11 11:13:19 -0600

    journal entry CRUD working again. had to add the path in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,17 @@ const routes: Routes = [
     path: 'reset-password',
     loadChildren:
       './pages/reset-password/reset-password.module#ResetPasswordPageModule'
+  },
+  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  {
+    path: 'details/:id',
+    loadChildren:
+      './pages/journal-details/journal-details.module#JournalDetailsPageModule'
+  },
+  {
+    path: 'details',
+    loadChildren:
+      './pages/journal-details/journal-details.module#JournalDetailsPageModule'
   }
 ]
 
