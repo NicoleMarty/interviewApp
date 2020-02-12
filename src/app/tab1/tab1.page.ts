c029f9b8734266ff1438ef3c3b883d5c71a6f2fa
Nicole Marty <nmarty310@gmail.com>
2020-02-11 22:58:54 -0600

    fixing an auth issue

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { AuthenticationService } from '../shared/auth.service'
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-  constructor () {}
+export class Tab1Page implements OnInit {
+  constructor (public authService: AuthenticationService) {}
+
+  ngOnInit () {}
 }
