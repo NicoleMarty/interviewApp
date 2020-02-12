4b98b43f463d8b0d0bbc1fecd196921ed03e2672
Nicole Marty <nmarty310@gmail.com>
2020-02-12 02:01:46 -0600

    importing font and color theme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,7 +4,7 @@ import { AuthenticationService } from '../shared/auth.service'
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.css']
 })
 export class Tab1Page implements OnInit {
   constructor (public authService: AuthenticationService) {}
